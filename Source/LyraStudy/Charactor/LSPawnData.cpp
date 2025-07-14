99c967e8ed8c4a3a5f412018ea25d841067e1cc3
Jungdragon <aszd0708@gmail.com>
2025-07-14 16:27:34 +0900

    git lfs 설정, 폴더 오타 수정

--- Source/LyraStudy/Charactor/LSPawnData.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "LSPawnData.h"
-
-ULSPawnData::ULSPawnData(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-	
-}
